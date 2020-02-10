b27679971291446644876de60743d7275f65985f
Brandon Hum <brandon.hum88@gmail.com>
2020-02-09 21:26:03 -0500

    React scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -97,4 +99,6 @@
             </div>
         </div>
     </body>
+
+    <script type="text/javascript" src="js/app.js"></script>
 </html>
